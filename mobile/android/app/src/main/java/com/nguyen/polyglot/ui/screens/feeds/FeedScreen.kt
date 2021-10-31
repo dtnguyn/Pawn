b00c1a44a25da92137b86546a1e380d2b7c11436
Adron Nguyen <adron0914@gmail.com>
2021-10-31 16:29:51 -0400

    Refactor custom feed dialog and add close button

--- mobile/android/app/src/main/java/com/nguyen/polyglot/ui/screens/feeds/FeedScreen.kt
+++ mobile/android/app/src/main/java/com/nguyen/polyglot/ui/screens/feeds/FeedScreen.kt
@@ -2,7 +2,6 @@ package com.nguyen.polyglot.ui.screens
 
 import android.os.Build.VERSION.SDK_INT
 import android.util.Log
-import android.widget.Space
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,40 +11,25 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import coil.ImageLoader
-import coil.compose.rememberImagePainter
 import coil.decode.GifDecoder
 import coil.decode.ImageDecoderDecoder
-import com.nguyen.polyglot.R
 import com.nguyen.polyglot.model.Feed
 import com.nguyen.polyglot.model.Language
 import com.nguyen.polyglot.ui.SharedViewModel
-import com.nguyen.polyglot.ui.components.RoundButton
 import com.nguyen.polyglot.ui.components.feed.*
 import com.nguyen.polyglot.ui.navigation.PolyglotScreens
 import com.nguyen.polyglot.ui.screens.feeds.FeedViewModel
 import com.nguyen.polyglot.ui.theme.*
 import com.nguyen.polyglot.util.DataStoreUtils
 import com.nguyen.polyglot.util.UIState
-import com.nguyen.polyglot.util.UtilFunctions
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -72,9 +56,9 @@ fun FeedScreen(
 
     var isLoading by remember { mutableStateOf(false) }
 
-    var addDialog by remember { mutableStateOf(false) }
-    var addUrl by remember { mutableStateOf("") }
-    var addType by remember { mutableStateOf("news") }
+    var customFeedDialog by remember { mutableStateOf(false) }
+    var customFeedUrl by remember { mutableStateOf("") }
+    var customFeedType by remember { mutableStateOf("news") }
 
 
     val imageLoader = ImageLoader.invoke(context).newBuilder()
@@ -287,115 +271,14 @@ fun FeedScreen(
 
             }
 
-            if (addDialog) {
-                Dialog(onDismissRequest = { addDialog = false }) {
-                    Surface(
-                        modifier = Modifier.fillMaxWidth(),
-                        shape = RoundedCornerShape(15.dp)
-                    ) {
-
-                        Column(Modifier.padding(15.dp)) {
-                            Text(
-                                text = "Custom Feed",
-                                style = Typography.h3,
-                                modifier = Modifier.align(CenterHorizontally)
-                            )
-                            Image(
-                                painter = rememberImagePainter(
-                                    data = if(addType == "news") R.drawable.news_gif else R.drawable.video_gif,
-                                    imageLoader = imageLoader
-                                ),
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .size(256.dp)
-                                    .align(CenterHorizontally)
-                            )
-                            Text(
-                                text = "Add news or video url from outside source",
-                                fontSize = 18.sp,
-                                style = Typography.body1,
-                                modifier = Modifier.padding(5.dp)
-                            )
-                            Spacer(Modifier.padding(3.dp))
-                            Row {
-                                Text(
-                                    text = "News",
-                                    color = Color.White,
-                                    style = Typography.body1,
-                                    modifier = Modifier
-                                        .background(
-                                            if (addType == "news") DarkBlue else Grey,
-                                            RoundedCornerShape(40.dp)
-                                        )
-                                        .clip(RoundedCornerShape(40.dp))
-                                        .clickable {
-                                            addType = "news"
-                                        }
-                                        .padding(10.dp)
-                                )
-                                Spacer(Modifier.padding(3.dp))
-                                Text(
-                                    text = "Video",
-                                    color = Color.White,
-                                    style = Typography.body1,
-                                    modifier = Modifier
-                                        .background(
-                                            if (addType == "video") DarkBlue else Grey,
-                                            RoundedCornerShape(40.dp)
-                                        )
-                                        .clip(RoundedCornerShape(40.dp))
-                                        .clickable {
-                                            addType = "video"
-                                        }
-                                        .padding(10.dp)
-                                )
-                            }
-                            Spacer(modifier = Modifier.padding(5.dp))
-
-                            TextField(
-                                value = addUrl,
-                                onValueChange = { newValue -> addUrl = newValue },
-                                label = {
-                                    Text(
-                                        text = "Enter url",
-                                        style = Typography.body2,
-                                        color = Color.Gray
-                                    )
-                                },
-                                singleLine = true,
-                                colors = TextFieldDefaults.textFieldColors(
-                                    focusedIndicatorColor = Color.Transparent,
-                                    unfocusedIndicatorColor = Color.Transparent,
-                                    disabledIndicatorColor = Color.Transparent
-                                ),
-                                shape = RoundedCornerShape(20.dp),
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .aspectRatio(5f)
-                            )
-
-
-                            Spacer(modifier = Modifier.padding(10.dp))
-
-                            Button(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .aspectRatio(5f),
-                                colors = ButtonDefaults.buttonColors(ReallyRed),
-                                shape = RoundedCornerShape(15.dp),
-                                onClick = { /*TODO*/ }) {
-                                Text(
-                                    text = "Add",
-                                    color = Color.White,
-                                    style = Typography.h4,
-                                    modifier = Modifier.align(CenterVertically)
-                                )
-                            }
-
-                        }
-                    }
-
-                }
+            if (customFeedDialog) {
+                CustomFeedDialog(
+                    customUrl = customFeedUrl,
+                    currentType = customFeedType,
+                    imageLoader = imageLoader,
+                    onDismiss = { customFeedDialog = false },
+                    onHandleUrlChange = { customFeedUrl = it },
+                    onChangeType = { customFeedType = it })
             }
 
             Box(
@@ -410,7 +293,7 @@ fun FeedScreen(
                         .size(56.dp),
                     shape = CircleShape,
                     onClick = {
-                        addDialog = true
+                        customFeedDialog = true
                     }
                 ) {
                     Icon(Icons.Filled.Add, "")
